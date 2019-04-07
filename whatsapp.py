749b7e1056404ea6f46a61007b9db6f827703376
athy125 <40031245+athy125@users.noreply.github.com>
2019-04-08 00:40:07 +0530

    Add files via upload

---
+++ whatsapp.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+
+driver = webdriver.firefox()
+driver.get('https://web.whatsapp.com/')
+
+name = input('Enter the name of User or Group: ')
+msg = input('Enter your message: ')
+count = int(input('Enter the count: '))
+
+input('Enter anything after scanning QR code: ')
+
+user = driver.find_element_by_xpath('//span@title = "{}"]'.format(name))
+user.click()
+
+msg_box = driver.find_element_by_class_name('input_container')
+
+for i in range(count):
+	msg_box.send_keys(msg)
+	button = driver.find_element_by_class_name('compose-btn-send')
+	button.click()
+
+
